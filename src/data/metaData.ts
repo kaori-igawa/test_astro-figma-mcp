c49454cbad1ee9d5113f1c95a9fcef5e80ca1c27
kaori-igawa <kaori0v0igawa@gmail.com>
2025-05-15 11:51:32 +0900

    meta setting

---
+++ src/data/metaData.ts
@@ -0,0 +1,9 @@
+import type { MetaDataType } from '@/types/common'
+
+export const metaData: MetaDataType = {
+  url: 'https://hogehoge/',
+  title: 'タイトル',
+  description: 'サイトの説明', 
+  keywords: 'aaa,bbb,ccc',
+  ogpImage: 'ogp.png',
+}
---
+++ src/types/common.ts
@@ -0,0 +1,7 @@
+export type MetaDataType = {
+  url: string;
+  title: string;
+  description: string;
+  keywords: string;
+  ogpImage: string;
+}
