ed48f677ff4160f094fa7a4694fcc44d389e8769
kaori-igawa <kaori0v0igawa@gmail.com>
2025-05-16 17:06:48 +0900

    メニューの開閉実装

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import dehazeIcon from '@/assets/dehaze.svg';
 
+import { Menu } from  '@/components/Menu/Menu'
+
 export const Header: React.FC = () => {
+  const [isOpenMenu, setIsOpenMenu] = useState<boolean>(false);
+
   return (
-    <header className="flex justify-between items-center w-full px-4 py-3 shadow-[0_0_2px_#E60012]">
-      <img src={dehazeIcon.src} alt="Logo" className="h-10 w-auto object-contain" />
-      <button className="p-2 flex items-center justify-center hover:opacity-80">
-        <img src={dehazeIcon.src} alt="Menu" className="w-6 h-6" />
+    <header className="
+    relative flex justify-between items-center 
+    w-full 
+    px-[calc(100vw*(16/375))] py-[calc(100vw*(12/375))] 
+    border-b-[calc(100vw*(2/375))] border-b-[#E60012]
+    ">
+      <img src={dehazeIcon.src} alt="Logo" className="h-[calc(100vw*(40/375))] w-auto object-contain" />
+      <button onClick={() => setIsOpenMenu(!isOpenMenu)} className="p-2 flex items-center justify-center hover:opacity-80">
+        <img src={dehazeIcon.src} alt="Menu" className="w-[calc(100vw*(24/375))] h-[calc(100vw*(24/375))]" />
       </button>
+
+      <Menu isOpenMenu={isOpenMenu} />
     </header>
   );
 }; 
---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+type PropsType = {
+  isOpenMenu: boolean;
+}
+
+export const Menu: React.FC<PropsType> = ({ isOpenMenu }) => {
+  return (
+    <menu className={`
+    absolute 
+    left-0
+    top-[calc(100vw*(66/375))]
+    transform 
+    overflow-hidden 
+    bg-white 
+    w-[100vw]
+    px-4 pt-5 pb-4 
+    text-left 
+    shadow-xl
+    transition-all transition-discrete duration-500 ease-fluid
+    opacity-0
+    h-0
+    [@starting-style]:opacity-0
+    [@starting-style]:h-0 
+    ${isOpenMenu && 'opacity-100 h-fit'}
+    `}>
+      <li>aaa</li>
+      <li>bbb</li>
+      <li>ccc</li>
+    </menu>
+  );
+}; 
